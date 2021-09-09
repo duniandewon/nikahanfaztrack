cb713e9f3deb2469be63aeaff8bef5c7cacc530f
Abolfaz <duniandewon@gmail.com>
2021-09-09 18:26:57 +0700

    Create footer

---
+++ components/elements/Footer/index.js
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>This website is designed and built by:</p>
+      <p>The bride and the groom</p>
+    </footer>
+  );
+};
+
+export default Footer;
