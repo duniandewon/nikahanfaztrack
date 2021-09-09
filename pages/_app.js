63ce3808dee87795b32ae4b172f4725471b320fc
Abolfaz <duniandewon@gmail.com>
2021-09-09 16:34:00 +0700

    Install and config fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
