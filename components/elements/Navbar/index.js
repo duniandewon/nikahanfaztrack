8a1d39823d72bf4e9009b8011cf500439c929c44
aboli <duniandewon@gmail.com>
2021-09-12 18:12:03 +0700

    Fix navbar
    Change navbar items to link.
    Give navbar container position relative and z-index 1

--- components/elements/Navbar/index.js
+++ components/elements/Navbar/index.js
@@ -5,6 +5,7 @@ import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { faGift } from "@fortawesome/free-solid-svg-icons";
 import { faEnvelopeOpenText } from "@fortawesome/free-solid-svg-icons";
 
+import Link from "next/link";
 import logo from "../../../public/icons/logo.png";
 
 import styles from "./Navbar.module.scss";
@@ -13,26 +14,34 @@ const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.navbar}>
-        <div className={styles.navbarItem}>
-          <FontAwesomeIcon icon={faHome} />
-          <span>beranda</span>
-        </div>
-        <div className={styles.navbarItem}>
-          <FontAwesomeIcon icon={faUsers} />
-          <span>vendor</span>
-        </div>
+        <Link href="/">
+          <a className={styles.navbarItem}>
+            <FontAwesomeIcon icon={faHome} />
+            <span>beranda</span>
+          </a>
+        </Link>
+        <Link href="/">
+          <a className={styles.navbarItem}>
+            <FontAwesomeIcon icon={faUsers} />
+            <span>vendor</span>
+          </a>
+        </Link>
       </div>
       <div className={styles.logo}>
         <Image src={logo} />
       </div>
       <div className={styles.navbar}>
-        <div className={styles.navbarItem}>
-          <FontAwesomeIcon icon={faGift} />
-          <span>hadiah</span>
-        </div>
-        <div className={styles.navbarItem}>
-          <FontAwesomeIcon icon={faEnvelopeOpenText} /> <span>ucapan</span>
-        </div>
+        <Link href="/">
+          <a className={styles.navbarItem}>
+            <FontAwesomeIcon icon={faGift} />
+            <span>hadiah</span>
+          </a>
+        </Link>
+        <Link href="/message">
+          <a className={styles.navbarItem}>
+            <FontAwesomeIcon icon={faEnvelopeOpenText} /> <span>ucapan</span>
+          </a>
+        </Link>
       </div>
     </div>
   );
