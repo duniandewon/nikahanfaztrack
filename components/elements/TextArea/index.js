763855502730fff596ef1ab8cc250edd2d401606
aboli <duniandewon@gmail.com>
2021-09-14 00:27:36 +0700

    Create textArea

---
+++ components/elements/TextArea/index.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+
+import styles from "../TextField/TextField.module.scss";
+
+const TextArea = (props) => {
+  const { id, name, value, placeholder, onChange, onKeyPress } = props;
+
+  return (
+    <div className={styles.container}>
+      <textarea
+        className={styles.input}
+        value={value}
+        id={id}
+        name={name}
+        placeholder={placeholder}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+      />
+    </div>
+  );
+};
+
+TextArea.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  onKeyPress: PropTypes.func,
+};
+
+export default TextArea;
