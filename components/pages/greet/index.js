25a3281641d5cc24538e466a8df31d5171a7e441
aboli <duniandewon@gmail.com>
2021-09-14 17:40:41 +0700

    Greeting page

--- components/pages/greet/index.js
+++ components/pages/greet/index.js
@@ -1,16 +1,44 @@
 import Wrapper from "../../layout/Wrapper";
-import Navbar from '../../elements/Navbar'
-import Footer from '../../elements/Footer'
-import Language from '../../elements/Language'
+import Navbar from "../../elements/Navbar";
+import Footer from "../../elements/Footer";
+import Language from "../../elements/Language";
+import TextField from "../../elements/TextField";
+import TextArea from "../../elements/TextArea";
+import Greetings from "../../elements/GreetingsList";
 
-import styles from './Greet.module.scss'
+import styles from "./Greet.module.scss";
+import { useState } from "react";
 
 const Greet = () => {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <Wrapper>
       <h1 className={styles.title}> send greetings</h1>
-      <Language/>
-      <Footer/>
+      <div className={styles.formContainer}>
+        <TextField
+          type="text"
+          id="name"
+          name="name"
+          placeholder="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextArea
+          placeholder="message"
+          id="message"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button disabled={!name || !message}>send</button>
+      </div>
+      <div className={styles.greetingsContainer}>
+        <Greetings />
+      </div>
+      <Language />
+      <Footer />
       <Navbar />
     </Wrapper>
   );
