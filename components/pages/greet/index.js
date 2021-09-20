5eb149e3a13b82809a8c7895e4c802106cf9a080
aboli <duniandewon@gmail.com>
2021-09-20 15:02:45 +0700

    Remove base url

--- components/pages/greet/index.js
+++ components/pages/greet/index.js
@@ -18,7 +18,7 @@ const Greet = () => {
   const [messages, setMessages] = useState([]);
 
   const { mutate } = useSWRConfig();
-  const { data } = useSWR("http://localhost:3000/api/greet");
+  const { data } = useSWR("/api/greet");
 
   const resetForm = () => {
     setName("");
@@ -27,14 +27,14 @@ const Greet = () => {
 
   const sendGreeting = async () => {
     mutate(
-      "http://localhost:3000/api/greet",
+      "/api/greet",
       { data: [...data.data, { name, message }] },
       false
     );
 
-    await axios.post("http://localhost:3000/api/greet", { name, message });
+    await axios.post("/api/greet", { name, message });
 
-    mutate("http://localhost:3000/api/greet");
+    mutate("/api/greet");
 
     resetForm();
   };
