a2f2207870f3c3bde6c801239c2f869e08cc6935
aboli <duniandewon@gmail.com>
2021-10-06 11:15:53 +0700

    Add bca qr code and faveicon

--- components/pages/gift/index.js
+++ components/pages/gift/index.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
 
 import en from "../../../public/locales/en/translate";
 import id from "../../../public/locales/id/translate";
@@ -7,16 +9,34 @@ import Wrapper from "../../layout/Wrapper";
 import Navbar from "../../elements/Navbar";
 import Language from "../../elements/Language";
 import Footer from "../../elements/Footer";
+import Modal from "../../layout/Modal";
+
+import Qrbca from "../../../public/images/qrbca.jpeg";
 
 import styles from "./Gift.module.scss";
 
 const Gift = () => {
+  const [showModal, setShowModal] = useState(false);
+
   const { locale } = useRouter();
 
   const t = locale === "en" ? en : id;
 
+  const _handleToggleModal = () => setShowModal(!showModal)
+
+  const renderModal = () => {
+    return (
+      <Modal show={showModal} onClose={_handleToggleModal}>
+        <div className={styles.qrbcaModal}>
+          <Image src={Qrbca} />
+        </div>
+      </Modal>
+    );
+  };
+
   return (
     <Wrapper title={t.navBar.gift}>
+      {renderModal()}
       <h1 className={styles.title}>{t.giftPage.title}</h1>
 
       <div className={styles.sendGift}>
@@ -35,7 +55,8 @@ const Gift = () => {
         </p>
 
         <p className={styles.bca}>
-          <span>jenius {t.giftPage.acct} M. Baragbah</span> <span>90012014737</span>
+          <span>jenius {t.giftPage.acct} M. Baragbah</span>{" "}
+          <span>90012014737</span>
         </p>
 
         <p className={styles.paypal}>
@@ -48,6 +69,10 @@ const Gift = () => {
             https://www.paypal.me/nanhanan
           </a>
         </p>
+      </div>
+
+      <div className={styles.qrbca} onClick={_handleToggleModal}>
+        <Image src={Qrbca} />
       </div>
 
       <Footer />
